452eb8b24e71cce008cc76601dcaaa6545b81fc5
Muhammad Wafaul Himam <wafaul.himam@gmail.com>
2018-12-18 11:16:51 +0700

    edit home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,7 @@ import logoImg from '../logo.png'
 
 export default () => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
+    <h1 style={{ textAlign: 'center' }}>Selamat datang</h1>
     <img src={logoImg} alt="" />
   </div>
 )
